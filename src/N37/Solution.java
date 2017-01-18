7bf7e3f5241932d82e4f72b2dafd791aeea5f952
Terence <wangtaowxs@gmail.com>
2017-01-18 22:42:26 +0100

    37, back-trace

---
+++ src/N37/Solution.java
@@ -0,0 +1,66 @@
+package N37;
+
+/**
+ * Created by terence on 1/18/17.
+ */
+public class Solution {
+    public static void main(String args[]) {
+        Solution s = new Solution();
+        String b[]={".........","......3..","...18....","...7.....","....1.97.",".........","...36.1..",".........",".......2."};
+        char board[][]=new char[9][9];
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++)
+                board[i][j]=b[i].charAt(j);
+        }
+        s.solveSudoku(board);
+
+    }
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    public boolean solve(char[][] board){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(board[i][j]=='.'){
+                    for (int k = 0; k < 9; k++) {
+                        board[i][j]=(char)('1'+k);
+                        if(isValidSudoku(board)&&solve(board)) return true;
+                        board[i][j]='.';
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            boolean t=check(board[i]);
+            if(!t) return false;
+            char temp[]=new char[9];
+            for (int j = 0; j < 9; j++) {
+                temp[j]=board[j][i];
+            }
+            t=check(temp);
+            if(!t) return false;
+            for (int j = 0;j<9;j++){
+                temp[j]=board[3*(i/3)+j/3][3*(i%3)+j%3];
+
+            }
+            t=check(temp);
+            if(!t) {
+                return false;}
+        }
+        return true;
+    }
+    public boolean check(char[] s){
+        boolean []used=new boolean[9];
+        for (int i = 0 ;i < 9; i++) used[i]=false;
+        for (int i = 0; i < 9; i++) {
+            if(s[i]=='.') continue;
+            else if(!used[s[i]-'1']) used[s[i]-'1']=true;
+            else return false;
+        }
+        return true;
+    }
+}
