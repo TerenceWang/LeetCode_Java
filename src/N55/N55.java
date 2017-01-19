7c6fe635e7547bb618f0fc22e7e708bbc561bd44
Terence <wangtaowxs@gmail.com>
2017-01-19 19:10:29 +0100

    55

---
+++ src/N55/N55.java
@@ -0,0 +1,21 @@
+package N55;
+
+/**
+ * Created by terence on 1/19/17.
+ */
+public class N55 {
+    public static void main(String [] args){
+        int []a={3,2,1,0,4};
+        System.out.println(new N55().canJump(a));
+    }
+
+    public boolean canJump(int[] nums) {
+        int pos=0;
+        for (int i = 0; i < nums.length; i++) {
+            if(pos>=i)
+                pos=Math.max(pos,i+nums[i]);
+        }
+        return pos>=nums.length-1;
+
+    }
+}
